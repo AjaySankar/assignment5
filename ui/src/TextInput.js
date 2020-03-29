12800096f17c632202074951bbd2cc059f898f3f
Ajay Sankar <ajay0196@gmail.com>
2020-03-29 16:27:46 -0700

    Added Number and Text input components

---
+++ ui/src/TextInput.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+
+export default class TextInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: props.value }
+    this.onBlur = this.onBlur.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange({ target: { value = "" } }) {
+    this.setState({ value })
+  }
+
+  onBlur(e) {
+    const { onChange } = this.props
+    const { value } = this.state
+    onChange(e, value)
+  }
+
+  render() {
+    const { value } = this.state
+    return (
+      <input
+        type="text"
+        value={value}
+        onBlur={this.onBlur}
+        onChange={this.onChange}
+      />
+    )
+  }
+}
