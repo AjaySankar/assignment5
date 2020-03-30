91542957a402f496ceae1f9f39da409a5e487b34
Ajay Sankar <ajay0196@gmail.com>
2020-03-30 13:07:04 -0700

    Added update functionality

--- ui/src/UpdateForm.js
+++ ui/src/UpdateForm.js
@@ -1,15 +1,50 @@
 import React, { Component } from "react"
-import NumInput from "./NumberInput"
+import { graphql } from "react-apollo"
+import { gql } from "apollo-boost"
+import { Link } from "react-router-dom"
 import TextInput from "./TextInput"
+import NumInput from "./NumberInput"
 
 const RESET_VALUES = { name: "", price: "$", category: "Shirts", image: "" }
 
-export default class UpdateForm extends Component {
+const updateProductMutation = gql`
+  mutation updateProduct(
+    $id: Int!
+    $category: Category!
+    $name: String!
+    $price: Float!
+    $image: String!
+  ) {
+    updateProduct(
+      product: {
+        id: $id
+        category: $category
+        name: $name
+        price: $price
+        image: $image
+      }
+    ) {
+      id
+      category
+      name
+      price
+      image
+    }
+  }
+`
+
+class UpdateForm extends Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props
     this.state = {
-      product: { ...RESET_VALUES },
+      product: { ...RESET_VALUES, ...{}, ...{ id: parseInt(id, 10) } },
     }
   }
 
@@ -19,7 +54,36 @@ export default class UpdateForm extends Component {
     this.setState(({ product: prevProduct }) => {
       return { product: { ...prevProduct, ...{ [name]: value } } }
     })
-    window.console.log(`Changed value to ${value}`)
+  }
+
+  handleSave(e) {
+    const { product } = this.state
+    const { id, category, name, image, price } = product
+    const { updateProduct } = this.props
+    window.console.log("Before update in client")
+    window.console.log(product)
+    updateProduct({
+      variables: {
+        id,
+        category,
+        name,
+        price,
+        image,
+      },
+    })
+      // eslint-disable-next-line no-unused-vars
+      .then(({ data = {} }) => {
+        // reset the form values to blank after submitting
+        this.setState({
+          product: { ...RESET_VALUES },
+        })
+      })
+      .catch((error) => {
+        window.console.error(
+          `Error occured while update product: ${error || ""}`
+        )
+      })
+      .finally(() => e.preventDefault()) // prevent the form submit event from triggering an HTTP Post
   }
 
   render() {
@@ -42,10 +106,13 @@ export default class UpdateForm extends Component {
         <label>Image URL </label>
         <TextInput name="name" onChange={this.handleChange} value={name} />
         <TextInput name="image" onChange={this.handleChange} value={image} />
-        <input type="submit" value="Add Product" onClick={this.handleSave} />
+        <input type="submit" value="Update Product" onClick={this.handleSave} />
+        <Link to="/"> Go to Home </Link>
       </form>
     )
   }
 }
 
-// <h1> {`Update form for product ${this.props.match.params.id}`} </h1>
+export default graphql(updateProductMutation, { name: "updateProduct" })(
+  UpdateForm
+)
