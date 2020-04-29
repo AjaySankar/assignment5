14dae54442a8afbd82d92fdd1e8ceb667a382eaa
Ajay Sankar <ajay0196@gmail.com>
2020-04-28 17:43:54 -0700

    Added ProductClient module to reuse connection

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,16 @@
 import React from "react"
-import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "react-apollo"
 import { Route, Switch } from "react-router-dom"
-import { createHttpLink } from "apollo-link-http"
 import ProductList from "./ProductList"
 import ProductView from "./ProductView"
 import UpdateForm from "./UpdateForm"
+import ProductsClient from "./ProductsClient"
 
 import "./App.css"
 
-const client = new ApolloClient({
-  link: createHttpLink({
-    fetchOptions: {
-      method: "POST",
-    },
-  }),
-  uri: "http://localhost:3000/graphql",
-})
-
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={ProductsClient}>
       <div>
         <h1> My Company Inventory </h1>
         <h3> Showing all available products </h3>
